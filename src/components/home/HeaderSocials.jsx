8f62163a6be3a16c3fd1b8a805fa5b9566d05593
Alexandre Figueiredo <alexandre.mc.figueiredo@ctw.bmwgroup.com>
2024-02-26 15:04:12 +0000

    Home component

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HeaderSocials() {
+    return (
+        <div className="home__socials">
+            <a href="https://www.linkedin.com/in/alexfigas/" className="home__social-link" target="_blank" rel="noreferrer">
+                <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/AlexFigas" className="home__social-link" target="_blank" rel="noreferrer">
+                <i className="fa-brands fa-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default HeaderSocials;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./home.css";
 
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
+import Shapes from "./Shapes";
+
+// TODO: Change avatar to my own
+import Me from "../../assets/avatar-3.svg";
+
 function Home() {
     return (
-        <div>Home</div>
+        <section className="home container" id="home">
+            <div className="intro">
+                <img src={Me} alt="" className="home__img" />
+                <h1 className="home__name">Alexandre Figueiredo</h1>
+                <span className="home__education">I'm a Software Engineer</span>
+
+                <HeaderSocials />
+
+                <a href="#contact" className="btn">Contact Me</a>
+                <ScrollDown />
+            </div>
+
+            <Shapes />
+        </section>
     )
 }
 
