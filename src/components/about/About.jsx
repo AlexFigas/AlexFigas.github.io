fa163fcc13b41213c9ab1f169324c033b337a5db
Alexandre Figueiredo <alexandre.mc.figueiredo@ctw.bmwgroup.com>
2024-02-27 19:18:33 +0000

    About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,63 @@
 import React from "react";
 import "./about.css";
 
+import Me from "../../assets/avatar-3.svg";
+
 function About() {
     return (
-        <div>About</div>
+        <section className="about container section" id='about'>
+            <h2 className="section__title">About Me</h2>
+
+            <div className="about__container grid">
+                <img src={Me} alt="" className="about__img" />
+
+                <div className="about__data grid">
+                    <div className="about__info">
+                        <p className="about__description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia nec nisi semper pretium.
+                            Donec malesuada, ex et placerat scelerisque, est dolor vestibulum purus, nec facilisis libero orci.
+                        </p>
+                        {/* TODO: Add href to CV here */}
+                        <a href="." className="btn">Download CV</a>
+                    </div>
+
+                    <div className="about__skills grid">
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Development</h3>
+                                <span className="skills__number">80%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage development"></span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">C++</h3>
+                                <span className="skills__number">70%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage cpp"></span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Gaming</h3>
+                                <span className="skills__number">30%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage gaming"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
---
+++ src/components/about/AboutBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function AboutBox() {
+  return (
+    <div className="about__boxes grid">
+        <div className="about__box">
+            <i className="about__icon">
+                <div>
+                    <h3 className="about__title"></h3>
+                    <span className="about__subtitle"></span>
+                </div>
+            </i>
+        </div>
+    </div>
+  )
+}
+
+export default AboutBox
